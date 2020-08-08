b8a095a33166a74c7722c0b44a1653e55e003e76
n-toy <nonam3ismyname@gmail.com>
2020-08-08 15:03:04 -0700

    geoJSON add

--- Mapping_Lines/static/js/logic.js
+++ Mapping_Lines/static/js/logic.js
@@ -4,9 +4,9 @@ console.log("working");
 // Create the map object with a center and zoom level.
 let map = L.map("mapid", {
     center: [
-      37.6213, -122.3790
+      40.7, -94.5
     ],
-    zoom: 5
+    zoom: 4
   });
 
 
